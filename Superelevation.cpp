aa7391a2e1002eeb661606354f1e36bde93acd77
Dominique Gladeau <gladeau@outlook.com>
2026-07-18 11:23:12 -0600

    Version 5

--- Superelevation.cpp
+++ Superelevation.cpp
@@ -1,3 +1,63 @@
 #include "Superelevation.h"
 #include <iostream>
-void Superelevation::Run() { std::cout << "\nSUPERELEVATION\n----------------\nSuperelevation tools are ready for implementation.\n"; }
+
+#include "Constants.h"
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+Superelevation::Superelevation()
+{
+    speed = 0.0;
+    radius = 0.0;
+    friction = 0.0;
+
+    superelevation = 0.0;
+    superelevationPercent = 0.0;
+}
+
+void Superelevation::Run()
+{
+    cout << "\nSUPERELEVATION CALCULATOR\n";
+    cout << "-------------------------\n";
+
+    cout << "Design Speed (km/h): ";
+    cin >> speed;
+
+    cout << "Radius (m): ";
+    cin >> radius;
+
+    cout << "Side Friction Factor: ";
+    cin >> friction;
+
+
+    superelevation =
+        (speed * speed) /
+        (127.0 * radius)
+        - friction;
+
+    superelevationPercent =
+        superelevation * 100.0;
+
+        cout << fixed << setprecision(3);
+
+        cout << "\nRESULTS\n";
+        cout << "-------------------------\n";
+
+        cout << "Speed               : "
+            << speed << " km/h\n";
+
+        cout << "Radius              : "
+            << radius << " m\n";
+
+        cout << "Friction            : "
+            << friction << endl;
+
+        cout << "Superelevation      : "
+            << superelevation << endl;
+
+        cout << "Superelevation (%)  : "
+            << superelevationPercent
+            << " %\n";
+    }
--- Superelevation.h
+++ Superelevation.h
@@ -1,7 +1,26 @@
 #pragma once
 
+
 class Superelevation
 {
 public:
+
+    Superelevation();
+
     void Run();
+
+private:
+
+    void GetInput();
+
+    void Calculate();
+
+    void DisplayResults();
+
+    double speed;
+    double radius;
+    double friction;
+
+    double superelevation;
+    double superelevationPercent;
 };
